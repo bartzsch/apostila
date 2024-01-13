0723b3e5fdd7195887033d43d983aaa6014af3e8
bartzsch <123250281+bartzsch@users.noreply.github.com>
2024-01-13 11:23:50 -0300

    Add files via upload

---
+++ produto.js
@@ -0,0 +1,11 @@
+var inputTamanho = Document.querySelector('name=tamanho')
+var inputTamanho = Document.querySelector('name=valortamanho')
+function mostraTamanho(){
+outputTamanho.value = inputTamanho.value
+}
+inputTamanho.oniput = mostraTamanho
+
+
+
+
+
